356e827266e4d484b498eb03f5fc7b64c57eab54
Daniela Palumbo <d166301@dac.unicamp.br>
2018-04-07 14:49:48 -0300

    View example

---
+++ src/views/LoginView.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+export default class LoginView extends React.Component<any, any>  {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (<div>teste</div>);
+    );
+  }
+}
