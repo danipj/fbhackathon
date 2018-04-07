4b7c5c2dd76bb598068eb6d44a8f75793d0304a6
tainaturella <tainaturella@gmail.com>
2018-04-07 15:51:48 -0300

    FeedView certa

---
+++ src/views/FeedView.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class FeedView extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Teste de ver como que worka</p>
+      </div>);
+  }
+}
+
+export { LoginView };
