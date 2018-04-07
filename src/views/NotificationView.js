c3efe9c5ccb716e8d8f1dfc38d9d6d6b2d8cd941
Daniela Palumbo <d166301@dac.unicamp.br>
2018-04-07 16:40:52 -0300

    Mocking up notification view

--- src/views/NotificationView.js
+++ src/views/NotificationView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class NotificationView extends Component {
 
@@ -8,7 +9,13 @@ export default class NotificationView extends Component {
 
   render() {
     return (<div>
-      teste
+	    		<h1>Notifications</h1>
+	    		<ListGroup>
+	          		<ListGroupItem active tag="a" href="#" action>Cras justo odio</ListGroupItem>
+	          		<ListGroupItem tag="a" href="#" action>Dapibus ac facilisis in</ListGroupItem>
+			        <ListGroupItem tag="a" href="#" action>Morbi leo risus</ListGroupItem>
+			        <ListGroupItem tag="a" href="#" action>Porta ac consectetur ac</ListGroupItem>
+	          	</ListGroup>
             </div>
           );
   }
