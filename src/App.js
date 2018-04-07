04a19d2e8596fa47eaf95d950426318db19c48e3
Daniela Palumbo <d166301@dac.unicamp.br>
2018-04-07 18:32:22 -0300

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LoginView } from './views/LoginView';
 import { FeedView } from './views/FeedView';
 import { NotificationView } from './views/NotificationView';
+import { NavbarLinda } from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-       <BrowserRouter
-          forceRefresh={false}>
-          <Switch>
-            <Route exact path="/login" component={LoginView} />
-            <Route exact path="/notifications" component={NotificationView}/>
-            <Route component={FeedView}/>
-          </Switch>
-      </BrowserRouter>
+      <div>
+        <NavbarLinda />
+         <BrowserRouter
+            forceRefresh={false}>
+            <Switch>
+              <Route exact path="/login" component={LoginView} />
+              <Route exact path="/notifications" component={NotificationView}/>
+              <Route component={FeedView}/>
+            </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class NavbarLinda extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="primary" light expand="sm">
+          <NavbarBrand href="/">projeto loco</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/notifications">notifications</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Profile
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Logout
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export { NavbarLinda };
