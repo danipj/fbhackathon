0a6d1ef51f9709db4c83153699fe9622cadd6b02
Natalia Cruz <cruznataliasantos@gmail.com>
2018-04-07 19:52:29 -0300

    login instagram

--- src/views/FeedView.js
+++ src/views/FeedView.js
@@ -19,4 +19,4 @@ export default class FeedView extends Component {
   }
 }
 
-export { LoginView };
+export { FeedView };
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from "prop-types";
 
 export default class LoginView extends Component {
 
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
+
+    this.onLoginButtonClick = this.onLoginButtonClick.bind(this);
+  }
+
+  onLoginButtonClick(){
+      window.OAuth.initialize('Y9Xb9oKFDroP5k_SY5Dxtdp1OOI')
+
+      window.OAuth.popup('instagram').done(function(result) {      
+      
+      /*
+        save user session
+      */
+      localStorage.setItem("userLogged", result.user);
+      localStorage.setItem("accessToken", result.access_token);
+
+      /*
+        create or update user data
+      */
+      
+
+      /*
+        redirect
+      */
+      //this.context.router.history.push("/notifications");
+    })
   }
 
   render() {
-    return (<div>
-      teste
-            </div>
-          );
+    return (
+      <div className="container text-center">
+          <img className="mb-4" src="https://www.radiokorea.com/images/news/2017/08/14/265249/1.jpg" alt="" width="72" height="72" />
+
+          <h3>Login</h3>
+          <Button color="primary" onClick={this.onLoginButtonClick}>Sign in with Instagram</Button>{' '}
+
+          <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
+      </div>
+    );
   }
 }
 
