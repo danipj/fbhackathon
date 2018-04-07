4078af80af4769c738c47abe77d4d9d29a6e1b8f
Daniela Palumbo <d166301@dac.unicamp.br>
2018-04-07 14:53:43 -0300

    More view examples and router

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -8,7 +8,16 @@ export default class LoginView extends React.Component<any, any>  {
   }
 
   render() {
-    return (<div>teste</div>);
+    return (      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Teste de ver como que worka</p>
+      </div>);
     );
   }
 }
