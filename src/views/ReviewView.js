d9e5e8f3bb4ad8dcd32e576a20d70f26f844b9c4
Daniela Palumbo <d166301@dac.unicamp.br>
2018-04-08 02:34:51 -0300

    Review with saved audio playing

--- src/views/ReviewView.js
+++ src/views/ReviewView.js
@@ -10,11 +10,20 @@ export default class ReviewView extends Component {
     this.params = params;
   }
 
+  pega_audio(idfoto){
+    return (<article className='clip'>
+      <audio controls controlsList='nodownload' id={idfoto} src={localStorage.getItem(idfoto)}>
+      </audio></article>);
+  }
+
+
   render() {
     return (<div>
               <FeedItem value='teste' />
               <div style={{textAlign:'center'}}>
-              {this.params.id}
+              <section class="sound-return">
+              {this.pega_audio('foto1')}
+            </section>
                 <Button> Aprovar </Button>
                   <p />
                 <Button> Excluir </Button>
